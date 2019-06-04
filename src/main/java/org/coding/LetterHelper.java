c62f2ed78cc67814f3108ae2ac5a07a9fc4c37a8
luoweiqiang <luoweiqiang@163.com>
2019-06-04 11:55:09 +0800

    refactor letter combinations

--- src/main/java/org/coding/LetterHelper.java
+++ src/main/java/org/coding/LetterHelper.java
@@ -10,17 +10,41 @@ import java.util.*;
 public class LetterHelper {
     private final static String[] LETTER_MAP={"","","a b c","d e f","g h i","j k l","m n o","p q r s","t u v","w x y z"};
 
+    /**
+     * Letter combination
+     * @param letters
+     * @return String
+     */
     public static String combination(int[] letters){
         if (letters.length==0){
             return "";
         }
-        StringBuffer result=new StringBuffer();
-        List<Set<String>> setList=new LinkedList<>();
+        List<Set<String>> setList=getCalculateCartesianProductList(letters);
+        Set<List<String>> lists = Sets.cartesianProduct(setList);
+        return parseLetterCombination(lists);
+    }
+
+    /**
+     * 计算笛卡尔积
+     * @param letters
+     * @return
+     */
+    private static List<Set<String>> getCalculateCartesianProductList(int[] letters){
+        List<Set<String>> result=new LinkedList<>();
         for(int i=0;i<letters.length;i++){
             Set<String> strings=new LinkedHashSet<>(Arrays.asList(LETTER_MAP[letters[i]].split(" ")));
-            setList.add(strings);
+            result.add(strings);
         }
-        Set<List<String>> lists = Sets.cartesianProduct(setList);
+        return result;
+    }
+
+    /**
+     * 提取（笛卡尔积）字符串组合
+     * @param lists
+     * @return
+     */
+    private static String parseLetterCombination(Set<List<String>> lists){
+        StringBuffer result=new StringBuffer();
         for (List<String> itemList:lists){
             String combStr="";
             for (String item:itemList){
