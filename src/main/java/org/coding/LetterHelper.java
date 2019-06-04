ff0beee4f0939a57f5a8351dfd0ebe9387567028
luoweiqiang <luoweiqiang@163.com>
2019-06-04 11:39:54 +0800

    consider single letter with arrays contains multi letters,using guava to calculate Cartesian product

--- src/main/java/org/coding/LetterHelper.java
+++ src/main/java/org/coding/LetterHelper.java
@@ -1,5 +1,9 @@
 package org.coding;
 
+import com.google.common.collect.Sets;
+
+import java.util.*;
+
 /**
  * @author luoweiqiang
  */
@@ -7,6 +11,23 @@ public class LetterHelper {
     private final static String[] LETTER_MAP={"","","a b c","d e f","g h i","j k l","m n o","p q r s","t u v","w x y z"};
 
     public static String combination(int[] letters){
-        return LETTER_MAP[letters[0]];
+        if (letters.length==0){
+            return "";
+        }
+        StringBuffer result=new StringBuffer();
+        List<Set<String>> setList=new LinkedList<>();
+        for(int i=0;i<letters.length;i++){
+            Set<String> strings=new LinkedHashSet<>(Arrays.asList(LETTER_MAP[letters[i]].split(" ")));
+            setList.add(strings);
+        }
+        Set<List<String>> lists = Sets.cartesianProduct(setList);
+        for (List<String> itemList:lists){
+            String combStr="";
+            for (String item:itemList){
+                combStr+=item;
+            }
+            result.append(combStr).append(" ");
+        }
+        return result.toString().trim();
     }
 }
--- src/test/java/org/coding/LetterHelperTest.java
+++ src/test/java/org/coding/LetterHelperTest.java
@@ -7,6 +7,11 @@ import org.junit.Test;
  * @author luoweiqiang
  */
 public class LetterHelperTest {
+    @Test
+    public void empty_array_should_return_none(){
+        int[] zero={};
+        Assert.assertEquals("",LetterHelper.combination(zero));
+    }
 
     @Test
     public void single_letter_and_array_only_contains_zero_should_return_none(){
@@ -19,5 +24,26 @@ public class LetterHelperTest {
         int[] nine={9};
         String mapStr="w x y z";
         Assert.assertEquals(mapStr,LetterHelper.combination(nine));
+    }
+
+    @Test
+    public void single_letter_and_array_contains_two_letters_should_return_right(){
+        int[] letters={2,3};
+        String mapStr="ad ae af bd be bf cd ce cf";
+        Assert.assertEquals(mapStr,LetterHelper.combination(letters));
+    }
+
+    @Test
+    public void single_letter_and_array_contains_two_letters_but_one_is_zero_should_return_right(){
+        int[] letters={0,3};
+        String mapStr="d e f";
+        Assert.assertEquals(mapStr,LetterHelper.combination(letters));
+    }
+
+    @Test
+    public void single_letter_and_array_contains_three_letters_should_return_right(){
+        int[] letters={0,2,3};
+        String mapStr="ad ae af bd be bf cd ce cf";
+        Assert.assertEquals(mapStr,LetterHelper.combination(letters));
     }
 }
